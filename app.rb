1d40c9830c1da102bd731167bdb8d8edb8423a05
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-12 09:17:59 -0600

    got the Get route done

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
 
+    post '/team' do
+        "Team Name: #{params[:name]}"
+        "Coach: #{params[:coach]}"
+        "Point Guard: #{params[:pg]}"
+        "Shooting Guard: #{params[:sg]}"
+        "Small Forward: #{params[:sf]}"
+        "Power Foward: #{params[:pf]}"
+        "Center: #{params[:c]}"
+        
+        erb :team
+    end
 end
