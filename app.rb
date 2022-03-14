e92b505a7cce24b27f8af05fb352a49b41f6efa4
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-13 20:54:37 -0500

    added more files for practice.

--- app.rb
+++ app.rb
@@ -16,4 +16,19 @@ class App < Sinatra::Base
         @c = params[:c]
         erb :team
     end
+
+    get '/address' do
+        erb :address
+    end
+
+    post '/shipping' do
+        @first = params[:first]
+        @last_name = params[:last_name]
+        @address = params[:address]
+        @country = params[:county]
+        @zip_code = params[:zip_code]
+        @city = params[:city]
+        @state = params[:state]
+        erb :shipping
+    end
 end
